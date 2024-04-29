6c3a0b9b9447f59ed6baa2e319e9a69122b55a76
harsh lodhi <harshlodhi82@gmail.com>
2024-04-29 12:13:42 +0530

    Fixed code

--- src/app/shared/services/movie-services.ts
+++ src/app/shared/services/movie-services.ts
@@ -2,15 +2,45 @@ import { Movie } from '../models/movie-model';
 
 export class MovieService {
     private srchQuery = "";
-    
+
     private info: string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus obcaecati qui facere aliquam beatae! Sapiente sed accusantium numquam itaque ea.";
     moviesList: Array<Movie> = [
-        new Movie("1", "Avatar", this.info, "https://cnet3.cbsistatic.com/img/_c60tyhNYZ1ncjqXGsxiX3MRfH8=/1092x0/2019/01/11/b251bf04-5bf8-469a-be8d-79489551460b/avatar-2009.jpg"),
-        new Movie("2", "Avengers", this.info, "https://i.ytimg.com/vi/48fKIXlxaXk/maxresdefault.jpg"),
-        new Movie("3", "End Game", this.info, "https://assets1.ignimgs.com/2019/04/25/avengers-cast-1556221213216.jpg?fit=bounds&width=1280"),
-        new Movie("4", "Far From Home", this.info, "https://wegotthiscovered.com/wp-content/uploads/2019/07/Spider-Man-Far-From-Home-Mysterio-Post-Credits-Scene-670x335.jpg"),
-        new Movie("5", "Inception", this.info, "https://cdn.onebauer.media/one/empire-tmdb/films/27205/images/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg"),
-        new Movie("6", "MIB", this.info, "https://cdn3.movieweb.com/i/article/vFRqoTc5V0CWkQjBSkuVOptDtQakzv/798:50/Men-In-Black-International-Photo-Chris-Hemsworth-Tessa.jpg")
+        new Movie(
+          "1",
+          "Avatar",
+          this.info,
+          "https://phantom-marca.unidadeditorial.es/bc7ac39a324ba34f956063664881b200/resize/828/f/jpg/assets/multimedia/imagenes/2022/12/22/16717250934792.jpg"
+        ),
+        new Movie(
+          "2",
+          "Avengers",
+          this.info,
+          "https://i.ytimg.com/vi/48fKIXlxaXk/maxresdefault.jpg"
+        ),
+        new Movie(
+          "3",
+          "End Game",
+          this.info,
+          "https://assets1.ignimgs.com/2019/04/25/avengers-cast-1556221213216.jpg?fit=bounds&width=1280"
+        ),
+        new Movie(
+          "4",
+          "Far From Home",
+          this.info,
+          "https://static1.srcdn.com/wordpress/wp-content/uploads/2019/05/Spider-Man-Far-From-Home-3.jpg"
+        ),
+        new Movie(
+          "5",
+          "Inception",
+          this.info,
+          "https://img.etimg.com/thumb/width-1600,height-900,imgsize-62032,resizemode-75,msid-92973661/news/new-updates/christopher-nolans-plot-map-of-inception-has-twitter-guessing-the-end.jpg"
+        ),
+        new Movie(
+          "6",
+          "MIB",
+          this.info,
+          "https://cdn3.movieweb.com/i/article/vFRqoTc5V0CWkQjBSkuVOptDtQakzv/798:50/Men-In-Black-International-Photo-Chris-Hemsworth-Tessa.jpg"
+        )
     ];
 
     availID:number;
